6e55b13414540475b6f28a9e1dadb7441b3a06c1
Mit Ramesh <mithun.ramesh19@trinityschoolnyc.org>
2021-04-05 13:54:37 -0400

    Checkstyle added

--- src/main/java/edu/brown/cs/mramesh4/TripGraph/TripGraphEdgeComparator.java
+++ src/main/java/edu/brown/cs/mramesh4/TripGraph/TripGraphEdgeComparator.java
@@ -3,12 +3,12 @@ package edu.brown.cs.mramesh4.TripGraph;
 import java.util.Comparator;
 
 /**
- * This is a comparator to compare cityEdge
+ * This is a comparator to compare cityEdge.
  * @param <N> node to use
  * @param <E> type of edge to use
  */
 public class TripGraphEdgeComparator<N extends TripGraphNode<N, E>, E extends TripGraphEdge<N, E>>
-  implements Comparator<E> {
+    implements Comparator<E> {
 
   @Override
   public int compare(E a, E b) {
